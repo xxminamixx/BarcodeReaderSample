c1721eb5942b1ec5ffc311879411594ad67794a6
xxminamixx <pencil3215@gmail.com>
2019-01-09 13:52:47 +0900

    Initial Commit

---
+++ BarcodeReaderSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeReaderSample
+//
+//  Created by 南京兵 on 2019/01/09.
+//  Copyright © 2019 kyohei.minami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
